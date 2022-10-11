7d2b7059542fa9bd4ba2db481cbff9aac10558d4
Daniela Jurado Blandón <daniijuradob@gmail.com>
2022-10-11 10:32:42 -0500

    rutas

--- routes/report.js
+++ routes/report.js
@@ -3,25 +3,32 @@ userSchema = require("../models/user"),
 medicinesSchema = require("../models/medicines"),
 router = express.Router();
 
-router.get("/report", (request, response) => {
-    try {
-        const USERS = userSchema.find();
-        const MEDICINES = medicinesSchema.find();
-
-        let userFilter = USERS.map((elm) => {
-            return {full_name: `${elm.names} ${elm.surnames}`, cedula: elm.cc}
-        });
-
-        let medicineFilter = MEDICINES.map((elm) => {
-        return (elm.nameMedicine).toUpperCase().trim()
-        });
-        
-        let medicineNoDuplicate = [...new Set(medicineFilter)];
-
-        response.json([userFilter, medicineNoDuplicate]);
-    } catch(error) {
-        response.json({message: error})
-    }
+router.get("/reportUsers", (request, response) => {
+    const pipeline  = [
+        {
+          '$project': {
+            '_id': 0, 
+            'names': 1, 
+            'surnames': 1
+          }
+        }
+      ];
+    userSchema.aggregate(pipeline)
+    .then((data) => response.json(data))
+    .catch((error) => response.json({ message: error }));
+});
+router.get("/reportMedicines", (request, response) => {
+    const pipeline  = [
+        {
+          '$project': {
+            '_id': 0, 
+            'nameMedicine': 1
+          }
+        }
+      ];
+    medicinesSchema.aggregate(pipeline)
+    .then((data) => response.json(data))
+    .catch((error) => response.json({ message: error }));
 });
 
 module.exports = router;
